2e0dd3ee1ffebcc86a9a46a25e899ecbdfd327fc
Vitor França <145136523+viitor14@users.noreply.github.com>
2023-11-04 14:42:11 -0300

    add update e select

--- modelo_fisico.sql
+++ modelo_fisico.sql
@@ -54,34 +54,43 @@ foreign key(Cod_cli) references cliente (Cod_cli),
 foreign key (Cod_tipo_qua) references Tipo_quarto(Cod_tipo_qua),
 foreign key (Cod_ope) references Operadora (Cod_ope));
 
-Insert into Nacionalidade(Sigla,Nome)values
-('BR','Brasil'),
-('UK','Reino Unido');
+Insert into Nacionalidade(Sigla,Nome)
+values('BR','Brasil'),
+('UK','Reino Unido'),
+('AR','Argentina');
 
 insert into cliente(Nome_cli,Telefone,Cpf,Passaporte,Documento)values
 ('Juliana Gomes','985422265','65987544782','6325','685598'),
-('Vitor','985642549','36595844497','2654','985647');
-
+('Vitor Manoel','985642549','36595844497','2654','985647'),
+('Carlos Cesár','994563244','65874552478','9754','254987');
 insert into tem(Sigla,Cod_cli)values
-('BR','2'),
-('UK','1');
+('BR','1'),
+('UK','2'),
+('AR','3');
 
 insert into tipo_quarto(Cod_tipo_qua,Desc_tipo,Val_Tipo_qua)values
 ('1','Quarto Famalia','R$135,00'),
-('2','Quarto Casal','R$64,00');
-									
+('2','Quarto Casal','R$64,00'),
+('3','Quarto Casal','R$64,00');
+																		
 insert into Quarto(Num_qua,Andar,Cod_tipo_qua) values						
-(4,1,'2'),
-(6,1,'1');
+(6,1,'1'),
+(15,2,'2'),
+(2,1,'3');
 
 insert into operadora(Cod_ope,Nom_ope)values
 ('985','MasterCard'),
+('478','Visa'),
 ('354','MasterCard');
 
 insert into Reserva(Num_Res,Num_cartao,Qtd_dia,Dat_res,Dat_inic,_Status,Cod_cli,Cod_tipo_qua,Cod_ope)values
-('1','6632895637914527','14','2023-06-02','2023-07-01','Aguardando Pagamento',1,'1','985'),
-('2','5638788854321547','5','2023-05-14','2023-06-05','Pago',2,'2','354');
--- values('Num_Res','Num_cartao','Qtd_dia','Dat_res','Dat_inic','_Status','Cod_cli','Cod_tipo_qua','Cod_ope')
+('1','6632895637914527','14','2023-06-02','2023-06-15','Pago',1,'1','985'),
+('2','4585142555673487','10','2023-06-24','2023-12-26','Aguardando Pagamento',2,'1','478'),
+('3','5638788854321547','5','2023-05-14','2023-06-05','Pago',3,'2','354');
+
+update cliente set Nome_cli = 'Augusto Silva' where Nome_cli ='Carlos Cesár';
+
+delete from reserva where Cod_cli=3;
 
 select * from nacionalidade;
 select * from tem;
@@ -91,7 +100,7 @@ select * from Quarto;
 select * from operadora;
 select * from reserva;
 
-SELECT cliente.Cpf AS CPF, 								-- tabela1.coluna AS é o nome da coluna que vai ser 'substituido' quando der select
+SELECT cliente.Cpf AS CPF, 								
 cliente.Nome_cli AS Nome,
 cliente.Telefone,
 cliente.Passaporte,
@@ -104,8 +113,7 @@ reserva.Dat_inic AS Data_Iniciada,
 reserva._Status AS Situação,
 reserva.Cod_cli AS Codigo_Cliente,
 reserva.Cod_tipo_qua AS Codigo_Tipo_Quarto,
-reserva.Cod_ope AS Codigo_Operadora						-- antes do from nao coloca virgula
-FROM cliente 											-- tabela 1
-INNER JOIN reserva 										-- tabela 2
+reserva.Cod_ope AS Codigo_Operadora						
+FROM cliente 											
+INNER JOIN reserva 										
 ON cliente.Cod_cli = reserva.Cod_cli;
-
